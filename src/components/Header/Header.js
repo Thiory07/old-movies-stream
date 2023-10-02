e62008abca580c122b9e415c47a97e80a2d6307a
Thiory <gimenesferreira@gmail.com>
2023-10-02 14:54:11 -0300

    playing arround with componnents and CSS

---
+++ src/components/Header/Header.js
@@ -0,0 +1,6 @@
+export default function Header() {
+ return ( 
+ <header>
+  <h1> my header </h1>
+ </header>)
+}
---
+++ src/components/Timeline/Timeline.js
@@ -0,0 +1,13 @@
+export default function Timeline() {
+ return (
+  <div className="timeline">
+   <div className="entry">
+    <button className="btn btn-outline btn-primary">Year 1888</button>
+    <div className="movie"> 
+     <h2>A horse movie</h2>
+     <a href="/">Youtube video</a>
+    </div>
+   </div>
+  </div>
+ );
+}
